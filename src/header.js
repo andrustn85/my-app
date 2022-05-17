06b8f21d36ff7065d09012c2ca52c219569ab911
Andres R <77374408+andrustn85@users.noreply.github.com>
2022-05-17 12:40:44 +0200

    Add files via upload

--- src/header.js
+++ src/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AiOutlineMail } from "react-icons/ai";
-import { FaLinkedin } from "react-icons/fa";
+import profile from './assets/img/imgnordhavn.jpeg';
 
 export default function Header() {
     return (
-        <div className="header">
-        <img alt="yo" src="../Imagenes/imgnordhavn.jpeg" />
+        <div className="header" >
+        <img alt="yo" src={profile} />
 <h1> Andres R </h1>
 <h2> React Developer </h2>
 <a className="link" href="https://devandres.netlify.app/#home" target="_blank" rel="noopener noreferrer" > Portfolio 2.0 </a>
